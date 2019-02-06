eb8fd3b3c0f2fdbfbd3a34f747338489fc63f8a8
Pradeepa Gopinath <pgopinath@ccs.neu.edu>
2019-02-05 19:08:23 -0500

    Working

--- Assignment1/Perceptron.py
+++ Assignment1/Perceptron.py
@@ -36,9 +36,9 @@ def update_w_value(w, x):
                 m.append(x[row_number,:])
 
         #print(m)
-        for columnX in m:
-            second = np.multiply(lambda_val, columnX.T)
-            w = [x + y for x, y in zip(w, second)]
+        for rowX in m:
+            secondTerm = np.multiply(lambda_val, rowX.T)
+            w = [x + y for x, y in zip(w, secondTerm)]
 
         print("Iteration %d, total_mistake %d" % (i + 1, len(m)))
         if len(m) == 0:
@@ -56,19 +56,32 @@ def main():
 
     # adding ones to first column
     final_dataset = np.c_[np.ones(clean_dataset.shape[0]), clean_dataset]
-
     # print(final_dataset)
 
     # initialize w values
     w = np.random.normal(size=(5,1))
+    #print(w)
+
     # Remove labels to get x
     x = np.delete(final_dataset, final_dataset.shape[1] - 1, axis=1)
-    # print(final_dataset)
 
     updated_w = update_w_value(w, x)
     print("Classifier weights",w)
 
-
+    # calculating w_o
+    # allProducts =[]
+    # for row in x:
+    #     row = np.matrix(row)
+    #     updated_w = np.matrix(updated_w)
+    #     product = np.matmul(row, updated_w)
+    #     #print(product)
+    #     allProducts.append(product)
+    #
+    # w_o = 1- np.sum(allProducts)
+    w_o = updated_w[0]
+    updated_w = np.array(updated_w)
+    normalized_weights = [x / -w_o for x in updated_w]
+    print("Normalized weights are", normalized_weights[1:])
 
 
 if __name__ == "__main__":
