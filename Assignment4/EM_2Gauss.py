67404ee607978060c3daea786e12ab18c47b6947
Pradeepa Gopinath <pgopinath@ccs.neu.edu>
2019-04-04 23:11:02 -0400

    4th

--- Assignment4/EM_2Gauss.py
+++ Assignment4/EM_2Gauss.py
@@ -4,6 +4,8 @@ import random
 import math
 import ConfusionMatrix
 from sklearn.utils import shuffle
+from scipy.stats import multivariate_normal
+import matplotlib as plt
 
 
 def extract_full_dataset():
@@ -17,47 +19,89 @@ def main():
     # Extracting data
 
     X = extract_full_dataset()
-    #print(X.shape)
+    X = shuffle(X, random_state=0)
+    # print(X.shape)
 
     # Initialization
 
-    num_of_iterations = 500
+    num_of_iterations = 1500
     number_of_features = 2
     number_of_classification = 2
 
-
     # To check convergence
-    llh=[]
-
-
-    z_im = np.zeros((X.shape[0], number_of_classification))
+    llh = []
 
     # n * k (n : Number of features, k: number_of_classification)
-    mu = np.random.randint(min(X[:, 1]), max(X[:, 1]), size=(number_of_features, number_of_features))
-    print(mu.shape)
+    mu = np.random.randint(min(X[:, 0]), max(X[:, 0]), size=(number_of_features, number_of_features))
+    # print(mu.shape)
 
     # n * k * k
     cov = np.zeros((number_of_features, number_of_classification, number_of_classification))
-    #print(cov)
+    # print(cov)
+
+    # [0.5,0.5]
+    pi = np.ones(number_of_classification) / number_of_classification
 
     # Filling the diagonals
 
     for row in range(len(cov)):
-        np.fill_diagonal(cov[row], 4)
+        np.fill_diagonal(cov[row], 5)
 
-    #print(cov)
+    # print(cov)
 
+    for i in range(num_of_iterations):
+        # E step
 
-    # Algorithm implementation
-    # while True:
-    #     for i in range(number_of_features):
-    #         for m in range(number_of_classification):
-    #             break
+        z_im = np.zeros((len(X), len(cov)))
 
+        # Getting the z_im value that sum to one.
 
-        # if has_it_converged():
-        #     break
+        for mean, c, w, column_z in zip(mu, cov, pi, range(len(z_im[0]))):
+            print(mean.shape)
+            norm = multivariate_normal(mean=mean, cov=c)
+            prob_df = norm.pdf(x=X)
+            numerator = w * prob_df
 
+            # Sum of pdf of point belonging to
+            denominator = 0
+
+            for mean_k, pi_k, cov_k in zip(mu, pi, cov):
+                denominator += multivariate_normal(mean_k, cov_k).pdf(X) * pi_k
+            z_im[:, column_z] = numerator / denominator
+        # print(z_im)
+
+        # M step
+
+        # Resetting the values of mu, cov, pi
+        mu = []
+        cov = []
+        pi = []
+
+        for m in range(len(z_im[0])):
+            sum_zim = np.sum(z_im[:, m], axis=0)
+            mu_m = np.sum(X * z_im[:, m].reshape(len(X), 1), axis=0)
+
+            # mean
+            mu.append(mu_m / sum_zim)
+
+            # Cov
+            cov.append(np.dot((np.array(z_im[:, m]).reshape(len(X), 1) * (X - mu_m)).T, (X - mu_m)) / sum_zim)
+
+            # pi_m
+
+            pi.append(sum_zim / np.sum(z_im))
+
+        llh.append(np.log(np.sum(
+            [k * multivariate_normal(mu[i], cov[j]).pdf(X) for k, i, j in zip(pi, range(len(mu)), range(len(cov)))])))
+    print(llh)
+    print(mu)
+    print(cov)
+    count_n1 = 0
+    for first, second in z_im:
+        if first>second:
+            count_n1+=1
+    print("n1 = ", count_n1 ,"points")
+    print("n2 = ", 6000 - count_n1, "points")
 
 
 if __name__ == '__main__':
